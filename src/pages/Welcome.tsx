28cb151d8b7d942fc398528b67fcf9c20aa3bac6
Luiz Pires <lpiresjunior@gmail.com>
2021-04-20 09:21:02 -0300

    Refactor welcome screen

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Image, StyleSheet, Text } from 'react-native';
+import { Dimensions, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import wateringImg from '../assets/watering.png';
-import { Button } from '../components/Button';
 import colors from '../styles/colors';
 
 export function Welcome() {
-
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>
@@ -16,14 +14,16 @@ export function Welcome() {
         de forma fácil
       </Text>
 
-      <Image source={wateringImg} style={styles.image} />
+      <Image source={wateringImg} style={styles.image} resizeMode="contain" />
 
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas.
         Nós cuidamos de lembrar você sempre que precisar.
       </Text>
 
-      <Button title=">" />
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>{ '>' }</Text>
+      </TouchableOpacity>
 
     </SafeAreaView>
   )
@@ -42,13 +42,25 @@ const styles = StyleSheet.create({
     color: colors.heading,
   },
   image: {
-    width: 292,
-    height: 284,
+    height: Dimensions.get('window').width * 0.7,
   },
   subtitle: {
+    width: 360,
     textAlign: 'center',
     fontSize: 18,
-    paddingHorizontal: 20,
     color: colors.heading,
   },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 56,
+    height: 56,
+    backgroundColor: colors.green,
+    borderRadius: 16,
+    marginRight: 10,
+  },
+  buttonText: {
+    fontSize: 24,
+    color: colors.white,
+  }
 })
