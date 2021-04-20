bcbfef1272c68065e9ac06f427d08083b45206b0
Luiz Pires <lpiresjunior@gmail.com>
2021-04-20 09:57:26 -0300

    Configure expo google fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading'
+
+import { Jost_400Regular, Jost_600SemiBold, useFonts } from '@expo-google-fonts/jost';
 
 import { Welcome } from './src/pages/Welcome';
 
 export default function App() {
-  return (
-    <Welcome />
-  );
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if (!fontsLoaded)
+    return <AppLoading />
+
+  return <Welcome />
 }
 
