7dc9cd1f0c8d733f9483c4e66abbd976300d1008
Luiz Pires <lpiresjunior@gmail.com>
2021-04-21 11:56:48 -0300

    Create environments list in plant select screen

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import { EnvironmentButton } from '../components/EnvironmentButton';
 import { Header } from '../components/Header';
+import api from '../services/api';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+interface EnvironmentsProps {
+  key: string;
+  title: string;
+}
+
 export function PlantSelect() {
+  const [environments, setEnvironments] = useState<EnvironmentsProps[]>([]);
+
+  useEffect(() => {
+    async function fetchEnvironment() {
+      const { data } = await api.get('plants_environments');
+      setEnvironments([
+        { key: 'all', title: 'Todos'},
+        ...data
+      ]);
+    }
+
+    fetchEnvironment();
+  }, [])
 
   return (
     <SafeAreaView style={styles.container}>
@@ -16,6 +36,20 @@ export function PlantSelect() {
         <Text style={styles.title}>Em qual ambiente</Text>
         <Text style={styles.subtitle}>você quer colocar sua planta?</Text>
       </View>
+
+      <View>
+        <FlatList
+          data={environments}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.enviromentList}
+          renderItem={({ item }) => (
+            <EnvironmentButton title={item.title} />
+          )}
+        />
+      </View>
+
+      
     </SafeAreaView>
   )
 }
@@ -40,5 +74,10 @@ const styles = StyleSheet.create({
     color: colors.heading,
     fontFamily: fonts.text,
     lineHeight: 20,
+  },
+  enviromentList: {
+    height: 72,
+    justifyContent: 'center',
+    padding: 32,
   },
 })
