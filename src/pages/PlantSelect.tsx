c919e0a42b75a71db594fe232cc0711d06438c06
Luiz Pires <lpiresjunior@gmail.com>
2021-04-21 11:54:50 -0300

    Create header in plant select screen

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import { Header } from '../components/Header';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
 export function PlantSelect() {
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>PlantSelect</Text>
+      <View style={styles.header}>
+        <Header />
+
+        <Text style={styles.title}>Em qual ambiente</Text>
+        <Text style={styles.subtitle}>você quer colocar sua planta?</Text>
+      </View>
     </SafeAreaView>
   )
 }
@@ -13,5 +23,22 @@ export function PlantSelect() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: colors.background,
+  },
+  header: {
+    paddingHorizontal: 30,
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.text,
+    lineHeight: 20,
   },
 })
