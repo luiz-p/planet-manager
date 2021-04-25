b3e72bbdc64a7eca734a0eff56d0e1914345fea4
Luiz Pires <lpiresjunior@gmail.com>
2021-04-25 15:34:35 -0300

    Create save & load plants function

--- src/libs/stotage.ts
+++ src/libs/stotage.ts
@@ -1,3 +1,6 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { format } from "date-fns";
+
 export interface PlantProps {
   id: string;
   name: string;
@@ -10,4 +13,58 @@ export interface PlantProps {
     repeat_every: string;
   };
   dateFormatNotification: Date;
+}
+
+interface StoragePlantProps {
+  [id: string]: {
+    data: PlantProps;
+  }
+}
+
+export async function savePlant(plant: PlantProps): Promise<void> {
+  try {
+    const data = await AsyncStorage.getItem('@plantmanager:plants')
+    const oldPlants = data ? (JSON.parse(data) as StoragePlantProps) : {};
+
+
+    const newPlant = {
+      [plant.id]: {
+        data: plant
+      }
+    }
+
+    await AsyncStorage.setItem('@plantmanager:plants',
+      JSON.stringify({ ...newPlant, oldPlants })
+    );
+
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export async function loadPlant(): Promise<PlantProps[]> {
+  try {
+    const data = await AsyncStorage.getItem('@plantmanager:plants')
+    const plants = data ? (JSON.parse(data) as StoragePlantProps) : {};
+
+    const plantsSorted = Object
+      .keys(plants)
+      .map((plant) => {
+        return {
+          ...plants[plant].data,
+          hour: format(new Date(plants[plant].data.dateFormatNotification), 'HH:mm')
+        }
+      })
+      .sort((a, b) =>
+        Math.floor(
+          new Date(a.dateFormatNotification).getTime() / 1000 -
+          Math.floor(new Date(b.dateFormatNotification).getTime() / 1000)
+      ));
+
+    return plantsSorted;
+
+
+  } catch (error) {
+    throw new Error(error);
+  }
 }
